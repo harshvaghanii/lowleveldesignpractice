786bcea79c77e89ab19fb37e32bc472748b04bf7
harshvaghanii <harshvaghani98@gmail.com>
2024-10-06 16:36:00 -0400

    adding code in AutomatedSystem

--- ParkingLot/AutomatedSystem.java
+++ ParkingLot/AutomatedSystem.java
@@ -1,2 +1,39 @@
 public class AutomatedSystem {
+    private final int id;
+
+    public AutomatedSystem(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void openEntryBarrier() {
+
+    }
+
+    public void openExitBarrier() {
+
+    }
+
+    public int getAvailableSpot() {
+        return 0;
+    }
+
+    public ParkingTicket generateTicket() {
+        return null;
+    }
+
+    public String readVehicleNumber() {
+        return "";
+    }
+
+    public double calculateCharges() {
+        return 0.0;
+    }
+
+    public ParkingTicket scanTicket(int id) {
+        return null;
+    }
 }
